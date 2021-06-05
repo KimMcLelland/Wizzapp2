7a683a756e121fd7854420f854865927ba0b258b
Kim <faeriekim3@gmail.com>
2021-06-05 11:43:31 +0100

    movement and gravity achieved

---
+++ goblin.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+var speed : int = 300
+var vel : Vector2 = Vector2()
+var screen_size
+var gravity : int = 800
+var grounded : bool = false
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	vel.x += speed
+
+func _physics_process(delta):
+	var velocity = Vector2()
+	vel = move_and_slide(vel, Vector2.UP)
+	vel.y += gravity * delta
+	 
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	vel.x = -vel.x
--- wizard.gd
+++ wizard.gd
@@ -1,16 +1,32 @@
 extends KinematicBody2D
+ 
+var screen_size  
+var score : int = 0
+var speed : int = 400
+var jumpForce : int = 600
+var gravity : int = 800
+ 
+var vel : Vector2 = Vector2()
+var grounded : bool = false
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	var velocity = Vector2()
+	vel.x = 0
+
+# movement inputs
+	if Input.is_action_pressed("ui_left"):
+		vel.x -= speed
+	if Input.is_action_pressed("ui_right"):
+		vel.x += speed
+	vel = move_and_slide(vel, Vector2.UP)
+	vel.y += gravity * delta
+	if Input.is_action_pressed("jump") and is_on_floor():
+		vel.y -= jumpForce
+	position.x = clamp(position.x, 25, screen_size.x-25)
+	
+	
+
